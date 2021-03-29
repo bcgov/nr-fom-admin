fe3ba6ea53f30d9df2a983196386d77f9bef915e
Basil Vandegriend <basil.vandegriend@cgi.com>
2021-03-29 13:36:05 -0600

    Set up deploys for both demo and master (main)

--- openshift/templates/create-deploy-dev.ps1
+++ openshift/templates/create-deploy-dev.ps1
@@ -1,5 +1,10 @@
 Set-Variable -Name "name" -Value "admin"
-Set-Variable -Name "tag" -Value "demo"
 Set-Variable -Name "env" -Value "dev"
+
+Set-Variable -Name "tag" -Value "demo"
+oc delete all,NetworkPolicy -n a4b31c-$env -l template=fom-$name-deploy,tag=$tag
+oc process -f fom-$name-deploy.yml -p ENV=$env -p TAG=$tag | oc create -n a4b31c-$env -f -
+
+Set-Variable -Name "tag" -Value "main"
 oc delete all,NetworkPolicy -n a4b31c-$env -l template=fom-$name-deploy,tag=$tag
 oc process -f fom-$name-deploy.yml -p ENV=$env -p TAG=$tag | oc create -n a4b31c-$env -f -
