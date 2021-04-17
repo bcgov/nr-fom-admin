99a3a3cc0d0bda3d7544149e8f35c82c681a8b71
Basil Vandegriend <basil.vandegriend@cgi.com>
2021-04-17 12:30:18 -0600

    Reorg open shift, build triggers, fix dockerfile.

--- openshift/node-server/server.js
+++ openshift/node-server/server.js
@@ -2,11 +2,11 @@
 const express = require('express');
 var path = require('path');  
 
-const port = 4200;
+const port = 4300;
 const app = express();
 
 //Set the base path to the angular-test dist folder
-app.use(express.static(path.join(__dirname, 'nr-fom-admin/dist')));
+app.use(express.static(path.join(__dirname, 'nr-fom-public/dist')));
 
 app.listen(port, () => {
   console.log(`Server listening on the port::${port}`);
@@ -15,5 +15,5 @@ app.listen(port, () => {
 
 //Any routes will be redirected to the angular app
 app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'nr-fom-admin/dist/admin/index.html'));
+    res.sendFile(path.join(__dirname, 'nr-fom-public/dist/index.html'));
 });
