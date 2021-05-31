4ce1472907465661e5cd6f7124733cfed9e36c61
Ian Liu (po-yi.liu) <ianliuwk1019@gmail.com>
2021-05-31 15:09:41 -0700

    Add commenting scope for comment detail page

--- src/app/foms/review-comments/comment-detail/comment-detail.component.ts
+++ src/app/foms/review-comments/comment-detail/comment-detail.component.ts
@@ -1,8 +1,10 @@
 import {Component, Input} from '@angular/core';
 import {FormGroup} from '@angular/forms';
 import {RxFormBuilder} from '@rxweb/reactive-form-validators';
-import {ResponseCode} from 'core/api';
+import {CommentScopeCode, ResponseCode} from 'core/api';
 import {PublicCommentAdminResponse} from "core/api";
+import { StateService } from 'core/services/state.service';
+import * as _ from 'lodash';
 import {CommentDetailForm} from './comment-detail.form';
 
 @Component({
@@ -12,17 +14,18 @@ import {CommentDetailForm} from './comment-detail.form';
   exportAs: 'commentForm'
 })
 export class CommentDetailComponent {
+  commentScopeCodes: _.Dictionary<CommentScopeCode>;
   commentFormGroup: FormGroup;
   comment: PublicCommentAdminResponse;
   @Input() responseCodes: ResponseCode[]
 
   @Input() set selectedComment(comment: PublicCommentAdminResponse) {
-    console.log(comment)
     const commentFormGroup = new CommentDetailForm(comment)
     this.commentFormGroup = this.formBuilder.group(commentFormGroup)
     this.comment = comment;
   };
 
-  constructor(private formBuilder: RxFormBuilder) {
+  constructor(private formBuilder: RxFormBuilder, private stateSvc: StateService) {
+    this.commentScopeCodes = _.keyBy(this.stateSvc.getCodeTable('commentScopeCode'), 'code');
   }
 }
