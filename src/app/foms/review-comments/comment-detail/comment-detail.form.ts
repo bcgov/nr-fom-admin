fab09e93ac6771b118501027f63f092bdf55cada
Ian Liu (po-yi.liu) <ianliuwk1019@gmail.com>
2021-06-01 13:41:52 -0700

    review comment preset values

--- src/app/foms/review-comments/comment-detail/comment-detail.form.ts
+++ src/app/foms/review-comments/comment-detail/comment-detail.form.ts
@@ -1,4 +1,4 @@
-import {prop} from "@rxweb/reactive-form-validators"
+import {prop, required} from "@rxweb/reactive-form-validators"
 import {PublicCommentAdminResponse, PublicCommentAdminUpdateRequest, ResponseCode} from "../../../../core/api";
 
 const UPDATE_FIELDS = ['responseDetails', 'responseCode'] as const;
@@ -8,9 +8,12 @@ export class CommentDetailForm implements Pick<PublicCommentAdminUpdateRequest, 
   responseDetails: string = '';
 
   @prop()
+  @required()
   responseCode: string = '';
 
   constructor(comment: PublicCommentAdminResponse) {
+    this.responseCode = comment.response?.code;
+    this.responseDetails = comment.responseDetails;
   }
 
 }
