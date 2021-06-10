c20090adf6778770ab4e6d70e003356da7ede820
Basil Vandegriend <basil.vandegriend@cgi.com>
2021-06-10 10:31:33 -0600

    Get admin working on single hostname

--- openshift/create-admin-function.ps1
+++ openshift/create-admin-function.ps1
@@ -7,10 +7,13 @@ function DeleteAdminFrontEnd {
 }
 
 function CreateAdminFrontEnd {
-    param ($ApiVersion, $Suffix, $Env)
+    param ($ApiVersion, $Suffix, $Env, $ReplicaCount=2)
 
     DeleteAdminFrontEnd -Suffix $Suffix -Env $Env
 
-    Write-Output "Create Admin front-end for suffix $Suffix and env $Env using version $ApiVersion ..."
-    oc process -f fom-admin-deploy.yml -p ENV=$Env -p SUFFIX=$Suffix -p HOSTNAME="nr-fom-admin$Suffix" -p IMAGE_STREAM_VERSION=$ApiVersion | oc create -n a4b31c-$Env -f -
+    $Hostname="fom-nrs$Suffix.apps.silver.devops.gov.bc.ca"
+    # $Hostname="nr-fom-admin$Suffix.apps.silver.devops.gov.bc.ca" TODO: Old format hostname.
+
+    Write-Output "Create Admin front-end for suffix $Suffix and env $Env using version $ApiVersion with replica_count $ReplicaCount ..."
+    oc process -f fom-admin-deploy.yml -p ENV=$Env -p SUFFIX=$Suffix -p HOSTNAME=$Hostname -p IMAGE_STREAM_VERSION=$ApiVersion -p REPLICA_COUNT=$ReplicaCount | oc create -n a4b31c-$Env -f -
 }
