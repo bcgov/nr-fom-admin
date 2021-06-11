cc00b502d1cef00716466687c76b3a957ddbe5b8
Ian Liu (po-yi.liu) <ianliuwk1019@gmail.com>
2021-06-11 16:27:46 -0700

    - Show selected review item after saved and go to previous selected list and highlighted.

--- src/app/foms/review-comments/review-comments.component.ts
+++ src/app/foms/review-comments/review-comments.component.ts
@@ -9,6 +9,7 @@ import {
 } from 'core/api';
 import {ModalService} from 'core/services/modal.service';
 import {StateService} from 'core/services/state.service';
+import { CommentDetailComponent } from './comment-detail/comment-detail.component';
 
 // TODO: Not sure why we use this for error message...
 export const ERROR_DIALOG = {
@@ -31,11 +32,14 @@ export const ERROR_DIALOG = {
 export class ReviewCommentsComponent implements OnInit, OnDestroy {
 
   @ViewChild('commentListScrollContainer', {read: ElementRef})
-  public commentListScrollContainer: ElementRef; // TODO: Something is up with this...
- 
+  public commentListScrollContainer: ElementRef;
+  @ViewChild('commentDetailForm') 
+  commentDetailForm: CommentDetailComponent;
+
   public responseCodes = this.stateSvc.getCodeTable('responseCode')
   public loading = false;
   public projectId: number;
+  public selectedItem: PublicCommentAdminResponse;
 
   public data$: Observable<PublicCommentAdminResponse[]>;
   private ngUnsubscribe: Subject<boolean> = new Subject<boolean>();
@@ -66,6 +70,24 @@ export class ReviewCommentsComponent implements OnInit, OnDestroy {
     return this.commentSvc.publicCommentControllerFind(this.projectId);
   }
 
+  /**
+   * @param item item to be set to child component.
+   * @param pos scroll position (from the list). When user clicks, no need to save it, only until user click 'save' then 
+   *            the saveComment() method will call this to update again the selected item and set selected item to child
+   *            component and at the same time passing 'pos' to scroll to correct position for the list. Will need 
+   *            setTimeout to delay scrolling after view is good.
+   */
+  onReviewItemClicked(item: PublicCommentAdminResponse, pos: number) {
+    this.selectedItem = item;
+    this.commentDetailForm.selectedComment = item;
+    if (pos) {
+      // !! important to wait or will not see the effect.
+      setTimeout(() => { 
+        this.commentListScrollContainer.nativeElement.scrollTop = pos;
+      }, 100);
+    }
+  }
+
   ngOnDestroy() {
     this.ngUnsubscribe.next();
     this.ngUnsubscribe.complete();
@@ -78,15 +100,20 @@ export class ReviewCommentsComponent implements OnInit, OnDestroy {
     // TODO: error handling seems not quite right, need to revise it later.
     try {
       this.loading = true;
-      const result = await this.commentSvc.publicCommentControllerUpdate(id, update).toPromise()
+      const result = await this.commentSvc.publicCommentControllerUpdate(id, update).toPromise();
       if (result) {
-        const sbRef = this.modalSvc.openSnackBar({message: 'Comment saved', button: 'OK'});
-        sbRef.onAction().subscribe(() => {
-          // Comment is saved successfully, so triggering service to retrieve comment list 
-          // from backend for consistent state of the list at frontend.
-          this.commentSaved$.next();
-          this.loading = false;
-        });
+        // scroll position, important to get it first!!
+        const pos = this.commentListScrollContainer.nativeElement.scrollTop;
+
+        // Comment is saved successfully, so triggering service to retrieve comment list 
+        // from backend for consistent state of the list at frontend.
+        this.commentSaved$.next();
+        this.selectedItem = result; // updated selected.
+        this.loading = false;
+        setTimeout(() => {     
+          this.onReviewItemClicked(this.selectedItem, pos);
+        }, 100);
+
       } else {
         this.modalSvc.openDialog({data: {...ERROR_DIALOG, message: 'Failed to update', title: ''}})
         this.loading = false;
