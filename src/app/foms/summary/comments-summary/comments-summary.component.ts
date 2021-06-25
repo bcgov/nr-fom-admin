53abe100eb6f78656688ab4b0f4962e35e931f21
Ian Liu (po-yi.liu) <ianliuwk1019@gmail.com>
2021-06-25 16:17:05 -0700

    - Work out public comments sections for different categories for Summary.

--- src/app/foms/summary/comments-summary/comments-summary.component.ts
+++ src/app/foms/summary/comments-summary/comments-summary.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { PublicCommentAdminResponse } from 'core/api';
+import { PublicCommentAdminResponse, ResponseCodeEnum } from 'core/api';
 import { StateService } from 'core/services/state.service';
 import * as _ from 'lodash';
 
@@ -11,16 +11,36 @@ import * as _ from 'lodash';
 export class CommentsSummaryComponent implements OnInit {
 
   commentScopeCodes = _.keyBy(this.stateSvc.getCodeTable('commentScopeCode'), 'code');
+  publicComments: PublicCommentAdminResponse[] = [];
+  addressedPcs: PublicCommentAdminResponse[] = [];
+  consideredPcs: PublicCommentAdminResponse[] = [];
+  irrelevantPcs: PublicCommentAdminResponse[] = [];
+  noResponsePcs: PublicCommentAdminResponse[] = [];
 
-  @Input() 
-  publicComments: PublicCommentAdminResponse[];
   @Input() 
   requestError: boolean
 
   constructor(private stateSvc: StateService) { }
 
-  ngOnInit(): void { 
-    console.log("comments:", this.publicComments)
+  ngOnInit(): void { }
+
+  @Input() set publicCommentDetails(publicComments: PublicCommentAdminResponse[]) {
+    this.publicComments = publicComments;
+    this.publicComments.forEach((comment)=> {
+      const item = Object.assign({}, comment); // JSON.parse(JSON.stringify(comment))
+      if (comment.response?.code === ResponseCodeEnum.Addressed) {
+        this.addressedPcs.push(item);
+      }
+      else if (comment.response?.code === ResponseCodeEnum.Considered) {
+        this.consideredPcs.push(item);
+      }
+      else if (comment.response?.code === ResponseCodeEnum.Irrelevant) {
+        this.irrelevantPcs.push(item);
+      }
+      else {
+        this.noResponsePcs.push(item)
+      }
+    });
   }
 
 }
