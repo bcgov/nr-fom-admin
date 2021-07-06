4a7eafd214f1ccf16f14863efa7d42c2ad772534
Marcelo Medeiros <marcelo.medeiros@cgi.com>
2021-07-06 13:03:24 -0600

    FOM Edit - Resizing Upload area and refactoring fom-add-edit.component.html

--- src/core/components/file-upload-box/file-upload-box.component.ts
+++ src/core/components/file-upload-box/file-upload-box.component.ts
@@ -20,44 +20,21 @@ import * as R from 'remeda';
             <span class="material-icons">
               cloud_upload
             </span>
-            <div>{{ multipleFiles ? dragMultipleFileMessage : dragSingleFileMessage }}</div>
-            <div>or</div>
-            <div>
-              <a href="javascript:void(0)">Browse</a>
-            </div>
+            <div>{{ multipleFiles ? dragMultipleFileMessage : dragSingleFileMessage }} or  <a href="javascript:void(0)">Browse</a> </div>
           </ngx-dropzone-label>
           <ng-container *ngFor="let f of files">
-          <ngx-dropzone-image-preview ngx-dropzone-image-preview   ngProjectAs="ngx-dropzone-preview"   [removable]="true" (removed)="onRemove(f)" [file]="f" *ngIf="isImageType(f.type)">
-            <ngx-dropzone-label>{{ f.name }}</ngx-dropzone-label>
-          </ngx-dropzone-image-preview>
-          <ngx-dropzone-preview *ngIf="!isImageType(f.type)" (removed)="onRemove(f)" [removable]="true" >
-            <ngx-dropzone-label>{{ f.name }}</ngx-dropzone-label>
-
-          </ngx-dropzone-preview>
-</ng-container>
+            <ngx-dropzone-image-preview ngx-dropzone-image-preview   ngProjectAs="ngx-dropzone-preview"   [removable]="true" (removed)="onRemove(f)" [file]="f" *ngIf="isImageType(f.type)">
+              <ngx-dropzone-label>{{ f.name }}</ngx-dropzone-label>
+            </ngx-dropzone-image-preview>
+            <ngx-dropzone-preview *ngIf="!isImageType(f.type)" (removed)="onRemove(f)" [removable]="true" >
+              <ngx-dropzone-label>{{ f.name }}</ngx-dropzone-label>
+            </ngx-dropzone-preview>
+          </ng-container>
         </ngx-dropzone>
         <ng-container *ngIf="invalidTypeText">
           <span class="invalid-type-msg">{{ invalidTypeText }}</span>
         </ng-container>
       </div>
-<!--
-      <div class="file-list" [class.no-files-border]="noFiles">
-        <ng-container *ngFor="let file of files; index as i">
-          <app-file-row
-            [displayName]="title"
-            [fileTypeStr]="file.type"
-            [fileName]="file.name"
-            [docType]="fileType"
-            [date]="date"
-            [fileSize]="file.size"
-            [index]="i"
-            (remove)="onRemove($event)"
-          ></app-file-row>
-        </ng-container>
-        <ng-container *ngIf="noFiles">
-          <span class="no-files-text">A file must be selected for upload</span>
-        </ng-container>
-      </div> -->
     </div>
   `,
   styleUrls: ['./file-upload-box.component.scss'],
